853cc9c4225c32fbcc74b24d0b1f0b94e0151974
pared <pawelredzynski@gmail.com>
2019-05-14 18:33:02 -0700

    use pathogen, add nerdtree

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export VIM_HOME=~/.vim
+export VIMRC=~/.vimrc
+
+rm -rf VIM_HOME VIMRC
+
+mkdir ${VIM_HOME}
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cp .vimrc ${VIMRC}
+
+git clone https://github.com/scrooloose/nerdtree.git ${VIM_HOME}/bundle/nerdtree
+git clone https://github.com/xolox/vim-misc ${VIM_HOME}/bundle/vim-misc
+git clone https://github.com/xolox/vim-notes ${VIM_HOME}/bundle/vim-notes
+
