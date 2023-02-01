b333b27fb62c36b01f223b52a8c0287cd2916458
Paweł Redzyński <pawel.redzynski@intuitionmachines.com>
2023-02-01 14:18:35 +0100

    fix zshrc placement

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ set -xu
 
 sudo apt-get update
 sudo apt-get install zsh fzf
-echo 'source /usr/share/doc/fzf/examples/key-bindings.zsh' >> .zshrc
-
+echo 'source /usr/share/doc/fzf/examples/key-bindings.zsh' >> ~/.zshrc
 
 export VIM_HOME=~/.vim
 export VIMRC=~/.vimrc
